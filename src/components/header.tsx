55fa44235126e39b9f0f2eefc0c7c139ee7dd711
dambar08 <notdambar@gmail.com>
2025-04-20 12:23:40 +0000

    add: compnent label, form

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,49 @@
 "use client"
-import { Link } from "lucide-react";
+import { Bell, User } from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react"
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { Button } from "./ui/button";
-import { Input } from "./ui/input";
+import Link from "next/link";
 
 export function Header() {
+    const { data: session } = useSession();
     const t = useTranslations("HomePage");
-
+    const isLoggedIn = !!session?.user;
     return (
-        <header className="bg-gray-800 p-4">
-            <h1 className="text-2xl font-bold">{t("title")}</h1>
-            <Button asChild>
-                <Link href="/movie">
-                    Movies
-                </Link>
-            </Button>
-            <Button asChild>
-                <Link href="/Series">
-                    Series
-                </Link>
-            </Button>
+        <header className="bg-gray-800 p-4 flex items-center justify-between">
+            <div className="flex items-center">
+                <h1 className="text-2xl font-bold mr-4">{t("title")}</h1>
+                <Button asChild variant="ghost">
+                    <Link href="/movie">
+                        Movies
+                    </Link>
+                </Button>
+                <Button asChild variant="ghost">
+                    <Link href="/series">
+                        Series
+                    </Link>
+                </Button>
+            </div>
+
+            <div className="flex items-center space-x-4">
+                {isLoggedIn ? (
+                    <>
+                        <Button variant="ghost" size="icon">
+                            <Bell className="h-5 w-5" />
+                        </Button>
+                        <Button asChild variant="secondary">
+                            <Link href="/account" className="flex items-center">
+                                <User className="h-4 w-4 mr-2" />
+                                Account
+                            </Link>
+                        </Button>
+                    </>) : (
+                    <Button variant="secondary" onClick={() => signIn()}>
+                        <User className="h-4 w-4 mr-2" />
+                        Getting Started
+                    </Button>
+                )}
+            </div>
         </header>
-    )
+    );
 }
