2e016a4af324081d1591d84d593dc08dc6b8228e
dambar08 <notdambar@gmail.com>
2025-04-28 14:56:47 +0000

    add series page

---
+++ src/app/[locale]/(dashboard)/dashboard/series/page.tsx
@@ -0,0 +1,22 @@
+import { Link } from "@/i18n/navigation";
+import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { MetricsCard } from "@/components/metrics-card"
+import { StatsChart } from "@/components/stats-chart"
+import { MovieTable } from "@/components/movie-table"
+import { BarChart3, ChevronDown, Globe, Home, LayoutDashboard, LifeBuoy, Settings, Wallet } from "lucide-react"
+import { db } from "@/server/db/index";
+import { QUERIES } from "@/server/db/queries"
+
+export default async function Page() {
+    const moviesCount = (await QUERIES.getMoviesCount())[0]?.count || 0;
+    const seriesCount = (await QUERIES.getSeriesCount())[0]?.count || 0;
+    return (
+        <main className="p-6">
+            <div className="mt-6">
+                <MovieTable />
+            </div>
+        </main>
+    )
+}
